122e982c4fc1e5fc6c42523a36e6dc10d68431cc
Karno Sose <karnosose@gmail.com>
2020-03-21 23:02:33 +0400

    created header with menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <div className="App">Works!!!</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
 
 export default App;
