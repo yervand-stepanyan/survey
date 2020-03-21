686604698df47ded458a253e2d56dfa4ade2c62e
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-03-21 19:56:03 +0400

    Add import Footer in Main

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Home from '../Home';
+import Footer from '../../Components/Footer';
 
 function Main() {
   return (
     <div>
       <div>Main works!!!</div>
       <Home />
+      <Footer />
     </div>
   );
 }
