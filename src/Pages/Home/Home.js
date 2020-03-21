48c9e68d867118e17421e7456579b8ca45835c56
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-21 17:16:08 +0400

    Add OurPartnersBlock component, import in Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import HomeBlock from '../../Components/HomeBlock';
+import AdPanel from '../../Components/AdPanel';
+import OurPartnersBlock from '../../Components/OurPartnersBlock';
 
 function Home() {
   return (
     <div>
       <div>Home Works!!!</div>
       <HomeBlock />
+      <AdPanel />
+      <OurPartnersBlock />
     </div>
   );
 }
