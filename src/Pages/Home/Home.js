745b8f756489418122aa9cf67cc8c21bf13a8871
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-22 13:21:06 +0400

    Refactor Home.js imports

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import HomeBlock from '../../Components/HomeBlock';
 import AdPanel from '../../Components/AdPanel';
+import HomeBlock from '../../Components/HomeBlock';
 import OurPartnersBlock from '../../Components/OurPartnersBlock';
 
 function Home(props) {
