9fc39818331b65fc61e49e0329a5ed5e5d306011
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-23 20:26:36 +0400

    WIP create slider in OurPartnersBlock

--- src/Components/OurPartnersBlock/OurPartnersBlock.js
+++ src/Components/OurPartnersBlock/OurPartnersBlock.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Typography from '@material-ui/core/Typography';
+
+import betconstruct from '../../assets/images/partners/betconstruct.png';
+import helix from '../../assets/images/partners/helix.png';
+import picsart from '../../assets/images/partners/picsart.png';
+import sfl from '../../assets/images/partners/sfl.png';
+import teamable from '../../assets/images/partners/teamable.png';
+import workfront from '../../assets/images/partners/workfront_new.png';
+
+const PARTNER_IMAGES = [betconstruct, helix, picsart, sfl, teamable, workfront];
+
 function OurPartnersBlock(props) {
   const { classes } = props;
 
-  return <div className={classes.container}>OurPartnersBlock works!!!</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.titleWrapper}>
+        <Typography variant="h6">Our Partners</Typography>
+      </div>
+      <div>
+        <div className={classes.sliderWrapper}>
+          {PARTNER_IMAGES.map(image => (
+            <div key={image}>
+              <img src={image} alt={image} className={classes.partnerImage} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 OurPartnersBlock.propTypes = {
--- src/Components/OurPartnersBlock/OurPartnersBlock.style.js
+++ src/Components/OurPartnersBlock/OurPartnersBlock.style.js
@@ -3,13 +3,25 @@ import GLOBAL_STYLES from '../../Theme/GlobalStyles';
 export const styles = {
   container: {
     display: 'flex',
-    justifyContent: 'center',
+    flexDirection: 'column',
     borderRadius: `${GLOBAL_STYLES.borders.borderRadius.base}`,
     boxShadow: `${GLOBAL_STYLES.boxShadow.base}`,
     boxSizing: 'border-box',
-    height: '20%',
+    minHeight: '20%',
     marginTop: `${GLOBAL_STYLES.spacing.base}`,
     padding: `${GLOBAL_STYLES.spacing.small}`,
     width: '100%'
+  },
+  titleWrapper: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  sliderWrapper: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  partnerImage: {
+    width: '150px',
+    height: '150px'
   }
 };
