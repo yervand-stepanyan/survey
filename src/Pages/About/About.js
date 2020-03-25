87d1aade9aa305df0742be7ba114c5f127bb24ff
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-25 20:10:32 +0400

    Add prop-types to About component

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function About() {
-  return <div>About Page works!!!</div>;
+function About(props) {
+  const { classes } = props;
+
+  return <div className={classes.container}>About Page works!!!</div>;
 }
+
+About.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 export default About;
