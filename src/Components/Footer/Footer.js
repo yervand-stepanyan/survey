d96783913f9b43ce698ee02a3d2fe4f3c9aa5025
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-03-25 23:00:32 +0400

    Add Footer component and its styles

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import logo from '../../assets/images/logo/logo.png';
 
 function Footer(props) {
   const { classes } = props;
 
-  return <footer className={classes.container}>Hello from Footer</footer>;
+  const MENU_ITEMS = [
+    {
+      name: 'Create Survey',
+      path: 'create-survey'
+    },
+    {
+      name: 'Take Survey',
+      path: 'take-survey'
+    },
+    {
+      name: 'Results',
+      path: 'results'
+    },
+    {
+      name: 'About',
+      path: 'about'
+    },
+    {
+      name: 'Contacts',
+      path: 'contacts'
+    }
+  ];
+
+  return (
+    <Router>
+      <footer className={classes.container}>
+        <div className={classes.footer}>
+          <div className={classes.footerTopPart}>
+            <div className={classes.footerLogoContainer}>
+              <Link to="/">
+                <img src={logo} alt="logo-img" className={classes.footerLogo} />
+              </Link>
+            </div>
+            <div className={classes.footerMenuItemsContainer}>
+              {MENU_ITEMS.map(item => {
+                return (
+                  <div className={classes.menuItemContainer}>
+                    <Link
+                      to={`/${item.path}`}
+                      className={classes.menuItem}
+                      key={item.name}
+                    >
+                      {item.name}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className={classes.footerBottomPart}>
+            <div className={classes.footerBottom}>
+              <div>copyright © 2020 survey</div>
+              <div className={classes.madeBy}>made by ...</div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </Router>
+  );
 }
 
 Footer.propTypes = {
--- src/Components/Footer/Footer.style.js
+++ src/Components/Footer/Footer.style.js
@@ -3,10 +3,55 @@ import GLOBAL_STYLES from '../../Theme/GlobalStyles';
 export const styles = {
   container: {
     backgroundColor: `${GLOBAL_STYLES.color.mainColorDarkBlue}`,
-    position: 'fixed',
+    color: 'white',
+    position: 'relative',
     left: 0,
     bottom: 0,
     width: '100%',
-    display: 'flex'
+    display: 'flex',
+    marginTop: `${GLOBAL_STYLES.spacing.xLarge}`
+  },
+  footer: {
+    width: '100%',
+    padding: `${GLOBAL_STYLES.spacing.base}`,
+    textTransform: 'capitalize'
+  },
+  footerTopPart: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  footerLogo: {
+    cursor: 'pointer'
+  },
+  menuItemContainer: {
+    marginTop: `${GLOBAL_STYLES.spacing.large}`
+  },
+  menuItem: {
+    border: '1px solid transparent',
+    color: 'white',
+    minWidth: 100,
+    padding: `${GLOBAL_STYLES.spacing.small} ${GLOBAL_STYLES.spacing.base}`,
+    textAlign: 'center',
+    textDecoration: 'none',
+    '&:hover': {
+      border: `1px solid ${GLOBAL_STYLES.color.mainColorGreen}`,
+      borderRadius: `${GLOBAL_STYLES.borders.borderRadius.base}`,
+      color: `${GLOBAL_STYLES.color.mainColorGreen}`
+    }
+  },
+  footerBottomPart: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: `${GLOBAL_STYLES.spacing.xxLarge}`
+  },
+  footerBottom: {
+    width: '55%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    fontSize: '10px'
+  },
+  madeBy: {
+    paddingRight: `${GLOBAL_STYLES.spacing.base}`
   }
 };
