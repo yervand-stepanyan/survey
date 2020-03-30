a5d6a4f051975924bc2c6a417153e36019726deb
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-30 17:24:51 +0400

    Add QuestionSection

---
+++ src/Components/CreateSurveyBlock/QuestionSection/QuestionSection.style.js
@@ -0,0 +1,10 @@
+import GLOBAL_STYLES from '../../../Theme/GlobalStyles';
+
+export const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: `${GLOBAL_STYLES.spacing.small}`
+  }
+};
---
+++ src/Components/CreateSurveyBlock/QuestionSection/index.js
@@ -0,0 +1,6 @@
+import { withStyles } from '@material-ui/core';
+
+import QuestionSection from './QuestionSection';
+import { styles } from './QuestionSection.style';
+
+export default withStyles(styles)(QuestionSection);
