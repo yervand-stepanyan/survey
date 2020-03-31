4b4f32b54556250de0142ac0399f9a13c0a6806d
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-03-31 13:18:16 +0400

    Add surveyReducer

--- src/State/reducer.js
+++ src/State/reducer.js
@@ -4,6 +4,9 @@ const SET_TITLE = 'SET_TITLE';
 const EDIT_QUESTION = 'EDIT_QUESTION';
 const SET_QUESTION = 'SET_QUESTION';
 
+const ADD_TITLE = 'ADD_TITLE';
+const ADD_QUESTION = 'ADD_QUESTION';
+
 export function titleReducer(state, action) {
   switch (action.type) {
     case EDIT_TITLE:
@@ -21,6 +24,17 @@ export function questionReducer(state, action) {
       return { question: action.payload, isQuestion: false };
     case SET_QUESTION:
       return { question: action.payload, isQuestion: true };
+    default:
+      return state;
+  }
+}
+
+export function surveyReducer(state, action) {
+  switch (action.type) {
+    case ADD_TITLE:
+      return { ...state, title: action.payload };
+    case ADD_QUESTION:
+      return { ...state, question: action.payload };
     default:
       return state;
   }
