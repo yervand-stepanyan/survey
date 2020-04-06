2ea298b57740e5a9194aad518cbf1bc8c227cdda
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-06 23:54:12 +0400

    Add OptionsContainer, refactor RadiobuttonSection

---
+++ src/Components/CreateSurveyBlock/OptionsContainer/OptionsContainer.js
@@ -0,0 +1,110 @@
+import React, { useRef, useState } from 'react';
+import uuid from 'react-uuid';
+
+import Chip from '@material-ui/core/Chip';
+import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+
+import removeSpaces from '../../../Helpers/removeSpaces';
+import { useStyles } from './OptionsContainer.style';
+
+const INPUT_LABEL = 'Option';
+
+function OptionsContainer() {
+  const classes = useStyles();
+  const inputEl = useRef(null);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [option, setOption] = useState('');
+  const [options, setOptions] = useState([]);
+  const [chip, setChip] = useState({});
+
+  const handleChange = event => {
+    setOption(event.target.value);
+
+    if (removeSpaces(event.target.value)) setIsEmpty(true);
+    else setIsEmpty(false);
+  };
+
+  const handleSubmitOnEnter = event => {
+    if (event.key === 'Enter') {
+      const filteredOption = removeSpaces(option);
+
+      if (filteredOption) {
+        setOptions(
+          chip.id
+            ? options.map(opt =>
+                opt.id === chip.id
+                  ? { id: chip.id, option: filteredOption }
+                  : opt
+              )
+            : [...options, { id: uuid(), option: filteredOption }]
+        );
+
+        setOption('');
+
+        setChip({});
+      } else setIsEmpty(false);
+    }
+  };
+
+  const handleClick = chipToEdit => () => {
+    if (!option) {
+      setOption(chipToEdit.option);
+
+      setChip(chipToEdit);
+
+      setIsEmpty(true);
+
+      inputEl.current.focus();
+    }
+  };
+
+  const handleDelete = chipToDelete => () => {
+    setOptions(() => options.filter(opt => opt.id !== chipToDelete));
+  };
+
+  return (
+    <div className={classes.optionsContainer}>
+      <div className={classes.inputWrapper}>
+        <div className={classes.textFieldWrapper}>
+          <TextField
+            autoFocus
+            error={!isEmpty}
+            id="outlined-basic"
+            inputRef={inputEl}
+            label={INPUT_LABEL}
+            onChange={e => handleChange(e)}
+            onKeyDown={handleSubmitOnEnter}
+            value={option}
+            variant="outlined"
+            fullWidth
+          />
+        </div>
+      </div>
+      <div className={classes.chipsWrapper}>
+        <div className={classes.root}>
+          {options.map(opt => (
+            <Tooltip
+              title={opt.option}
+              TransitionComponent={Zoom}
+              arrow
+              key={opt.id}
+            >
+              <Chip
+                className={classes.chip}
+                clickable
+                color="primary"
+                label={opt.option}
+                onClick={handleClick(opt)}
+                onDelete={handleDelete(opt.id)}
+              />
+            </Tooltip>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OptionsContainer;
--- src/Components/CreateSurveyBlock/RadiobuttonSection/RadiobuttonSection.style.js
+++ src/Components/CreateSurveyBlock/RadiobuttonSection/RadiobuttonSection.style.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles(theme => ({
+export const useStyles = makeStyles({
   radiobuttonSectionContainer: {
     display: 'flex',
     flexDirection: 'column'
@@ -8,34 +8,5 @@ export const useStyles = makeStyles(theme => ({
   titleWrapper: {
     display: 'flex',
     justifyContent: 'center'
-  },
-  inputWrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: `${theme.customSpacing.large}`
-  },
-  textFieldWrapper: {
-    width: '60%'
-  },
-  chipsWrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: `${theme.customSpacing.large}`
-  },
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    border: `2px solid ${theme.color.mainColorGreen}`,
-    borderRadius: `${theme.borders.borderRadius.base}`,
-    boxSizing: 'border-box',
-    height: '114px',
-    overflowY: 'auto',
-    padding: `${theme.customSpacing.small}`,
-    width: '60%'
-  },
-  chip: {
-    margin: `${theme.customSpacing.small}`,
-    maxWidth: '100%'
   }
-}));
+});
