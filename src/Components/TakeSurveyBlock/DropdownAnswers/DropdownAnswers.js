c6c565d52db0f15edbaa9658c5eac9d7650da6a6
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-13 14:25:34 +0400

    Add DropdownAnswers simple onchange

--- src/Components/TakeSurveyBlock/DropdownAnswers/DropdownAnswers.js
+++ src/Components/TakeSurveyBlock/DropdownAnswers/DropdownAnswers.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
+// material ui
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 import { useStyles } from './DropdownAnswers.style';
 
 function DropdownAnswers({ answers, questionId }) {
   const classes = useStyles();
 
+  const [answer, setAnswer] = useState('');
+
+  const handleChange = event => {
+    setAnswer(event.target.value);
+  };
+
   return (
     <div className={classes.container}>
-      {answers.map(({ id, option }) => {
-        return (
-          <div key={id}>
-            <label htmlFor={id}>
-              <input type="radio" id={id} name={questionId} />
-              <span>{option}</span>
-            </label>
-          </div>
-        );
-      })}
+      <FormControl variant="outlined" className={classes.formControl}>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={answer}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {answers.map(({ id, option }) => {
+            return (
+              <MenuItem key={id} value={id}>
+                {option}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
     </div>
   );
 }
