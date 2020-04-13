b8538468105e2bc928947ab9e9dc2a479a2b6238
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-13 15:52:34 +0400

    Add RangeAnswers and simple onchange

---
+++ src/Components/TakeSurveyBlock/RangeAnswers/RangeAnswers.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// material ui
+import { Typography, Slider } from '@material-ui/core';
+
+import { useStyles } from './RangeAnswers.style';
+
+function RangeAnswers({ questionId, startValue, endValue, stepValue }) {
+  const classes = useStyles();
+
+  function valuetext(value) {
+    return `${value}`;
+  }
+
+  return (
+    <div className={classes.container}>
+      <Typography id="discrete-slider" gutterBottom>
+        Answer
+      </Typography>
+      <Slider
+        defaultValue={startValue}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={stepValue}
+        marks
+        min={startValue}
+        max={endValue}
+      />
+    </div>
+  );
+}
+
+RangeAnswers.propTypes = {
+  questionId: PropTypes.string.isRequired,
+  startValue: PropTypes.number.isRequired,
+  endValue: PropTypes.number.isRequired,
+  stepValue: PropTypes.number.isRequired
+};
+
+export default RangeAnswers;
