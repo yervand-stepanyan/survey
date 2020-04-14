fa6f9b848630908c8aadb59620e89e5f4323e880
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-14 19:23:50 +0400

    Remove data.js and SurveyAnswer component

--- src/Components/TakeSurveyBlock/SurveyAnswer/SurveyAnswer.style.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column'
-  }
-}));
