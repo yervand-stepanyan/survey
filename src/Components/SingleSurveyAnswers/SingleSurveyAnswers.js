427c2824de84bd60c56c7e3634f008d72e953536
Karno Sose <karnosose@gmail.com>
2020-04-16 23:16:42 +0400

    Add prop type for SingleSurveyAnswers component

--- src/Components/SingleSurveyAnswers/SingleSurveyAnswers.js
+++ src/Components/SingleSurveyAnswers/SingleSurveyAnswers.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useParams} from "react-router";
 
 function SingleSurveyAnswers () {
-    let { surveyId } = useParams();
+    const { surveyId } = useParams();
 
     // const { index } = match.params;
     return <div>{surveyId}</div>
+}
+
+SingleSurveyAnswers.propTypes = {
+    surveyId: PropTypes.number.isRequired,
 }
 
 export default SingleSurveyAnswers;
