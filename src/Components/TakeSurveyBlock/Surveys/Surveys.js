3e49ab65e808c97f951fc516b4f49f6216fbf600
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-23 14:13:14 +0400

    Refactor Survey component

--- src/Components/TakeSurveyBlock/Surveys/Surveys.js
+++ src/Components/TakeSurveyBlock/Surveys/Surveys.js
@@ -8,13 +8,9 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import PollIcon from '@material-ui/icons/Poll';
-import Fab from '@material-ui/core/Fab';
-import Tooltip from '@material-ui/core/Tooltip';
-import Zoom from '@material-ui/core/Zoom';
 // Components
 import Survey from '../Survey';
 
@@ -23,44 +19,22 @@ import { useStyles } from './Surveys.style';
 function Surveys({ surveys }) {
   const classes = useStyles();
 
-  const [tempSurveys, setTempsurveys] = useState(surveys);
-
-  const handleChangeSurveyStatus = id => {
-    const newTempSurveys = surveys.map(item => {
-      if (item.surveyId === id) {
-        item.open = !item.open;
-      }
-      return item;
-    });
-
-    setTempsurveys(newTempSurveys);
-  };
-
-  const isSomeSurveyOpen = tempSurveys.some(survey => survey.open === true);
-  const openSurvey = tempSurveys.find(survey => survey.open === true);
-  console.log(openSurvey);
-
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
-        {/* <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead> */}
         <TableBody>
-          {tempSurveys.map(row => (
+          {surveys.map(row => (
             <TableRow key={row.surveyId}>
               <TableCell component="th" scope="row">
                 {row.surveyName}
               </TableCell>
               <div className={classes.takeAndResultsCellsContainer}>
                 <TableCell align="right">
-                  <Button variant="contained" color="primary">
-                    Take Survey
-                  </Button>
+                  <Link to={`survey/${row.surveyId}`}>
+                    <Button variant="contained" color="primary">
+                      Take Survey
+                    </Button>
+                  </Link>
                 </TableCell>
                 <TableCell align="right">
                   <Button variant="contained" color="primary">
