6f2610380f9ce1b89830a1e6cc4330ae83587a7f
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-23 13:40:08 +0400

    Add Snackbar Alert separate component

---
+++ src/Components/Alert/Alert.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export default Alert;
