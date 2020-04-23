d6986284b6cbdf18881e8dae51217bce85908896
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-04-23 14:18:04 +0400

    Refactor Survey component

--- src/Components/TakeSurveyBlock/Survey/Survey.js
+++ src/Components/TakeSurveyBlock/Survey/Survey.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 // Components
 import SurveyQuestions from '../SurveyQuestions';
 
 import { useStyles } from './Survey.style';
 
-function Survey({ survey }) {
+function Survey({ surveys, match }) {
   const classes = useStyles();
+
+  const { id } = match.params;
+
+  const chosenSurvey = surveys.find(item => {
+    return item.surveyId === id;
+  });
 
   return (
     <div className={classes.container}>
-      <SurveyQuestions questions={survey.questions} />
+      <SurveyQuestions questions={chosenSurvey.questions} />
     </div>
   );
 }
 
 Survey.propTypes = {
-  survey: PropTypes.object.isRequired
+  surveys: PropTypes.array.isRequired,
+  match: PropTypes.object.isRequired
 };
 
-export default Survey;
+export default withRouter(Survey);
