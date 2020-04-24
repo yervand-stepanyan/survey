8cc4c1aaa882cde2b77c7a227f79e41a24970174
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-24 18:40:32 +0400

    Fix fetchData doDelete call bug

--- src/FetchAPI/fetchData.js
+++ src/FetchAPI/fetchData.js
@@ -1,4 +1,4 @@
-async function request(url, method, body) {
+async function request({ url, method, body, id }) {
   const fetchData = body
     ? {
         body: JSON.stringify(body) || '',
@@ -12,7 +12,7 @@ async function request(url, method, body) {
       };
 
   const response = await fetch(
-    `${process.env.REACT_APP_API_ROOT}/api/${url}`,
+    `${process.env.REACT_APP_API_ROOT}/api/${url}/${id || ''}`,
     fetchData
   );
 
@@ -20,13 +20,13 @@ async function request(url, method, body) {
 }
 
 export function doGet(url) {
-  return request(url, 'GET');
+  return request({ url, method: 'GET' });
 }
 
 export function doPost(url, body) {
-  return request(url, 'POST', body);
+  return request({ url, method: 'POST', body });
 }
 
-export function doDelete(url, body) {
-  return request(url, 'DELETE', body);
+export function doDelete(url, id) {
+  return request({ url, method: 'DELETE', id });
 }
