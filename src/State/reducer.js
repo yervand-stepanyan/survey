cf8087f48902f709cb20781dd78e18e17e75ad5b
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-25 19:33:41 +0400

    Add remove survey action in reducer.js

--- src/State/reducer.js
+++ src/State/reducer.js
@@ -2,6 +2,7 @@ const ADD_SURVEY = 'ADD_SURVEY';
 const ADD_SURVEY_ANSWER = 'ADD_SURVEY_ANSWER';
 const ADD_SURVEY_ANSWERS = 'ADD_SURVEY_ANSWERS';
 const ADD_SURVEYS = 'ADD_SURVEYS';
+const REMOVE_SURVEY = 'REMOVE_SURVEY';
 
 export function surveyReducer(state, action) {
   switch (action.type) {
@@ -9,6 +10,8 @@ export function surveyReducer(state, action) {
       return action.payload;
     case ADD_SURVEY:
       return [action.payload, ...state];
+    case REMOVE_SURVEY:
+      return action.payload;
     default:
       return state;
   }
