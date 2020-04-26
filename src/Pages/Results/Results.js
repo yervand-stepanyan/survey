0cdb1f48cbd15b40a495c483b2223d5bbdb345ca
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-26 22:24:16 +0400

    Add useParams and filter data in Results.js

--- src/Pages/Results/Results.js
+++ src/Pages/Results/Results.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
 
 import ResultsBlock from '../../Components/ResultsBlock';
+import SurveyContext from '../../State/context';
 import { useStyles } from './Results.style';
 
 function Results() {
   const classes = useStyles();
+  const { id } = useParams();
+  const { stateSurveyAnswer } = useContext(SurveyContext);
+  const answers = stateSurveyAnswer.filter(
+    surveyAnswers => surveyAnswers.survey.id === id
+  );
+  const survey = answers.length ? answers[0].survey : {};
 
   return (
     <div className={classes.container}>
-      <ResultsBlock />
+      <ResultsBlock answers={answers} survey={survey} />
     </div>
   );
 }
