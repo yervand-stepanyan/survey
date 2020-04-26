cf0b7f7ab1184db18c2e83fd2ddc3ab69803d6b7
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-26 20:20:04 +0400

    Add survey title in SurveyQuestions.js

--- src/Pages/TakeSurvey/TakeSurvey.js
+++ src/Pages/TakeSurvey/TakeSurvey.js
@@ -1,20 +1,20 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
-import SurveyQuestions from '../../Components/TakeSurveyBlock/SurveyQuestions';
 import SurveyContext from '../../State/context';
+import SurveyQuestions from '../../Components/TakeSurveyBlock/SurveyQuestions';
 import { useStyles } from './TakeSurvey.style';
 
 function TakeSurvey() {
   const classes = useStyles();
-  const { stateSurvey } = useContext(SurveyContext);
   const { id } = useParams();
-
-  const chosenSurvey = stateSurvey.find(item => item.id === id);
+  const { stateSurvey } = useContext(SurveyContext);
+  const survey = stateSurvey.find(item => item.id === id);
+  const { title, questions } = survey;
 
   return (
     <div className={classes.container}>
-      <SurveyQuestions questions={chosenSurvey.questions} />
+      <SurveyQuestions questions={questions} title={title} />
     </div>
   );
 }
