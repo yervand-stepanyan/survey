1cbc17c98e43a9c95cb573e8d7e241394e80f7af
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-04-27 22:59:50 +0400

    Change HashRouter in Main.js, add base route in Routes.js

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useReducer, useEffect } from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Redirect,
   Route,
   Switch
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,9 @@
 const ROUTES = {
-  about: '/about',
-  auth: '/',
-  contacts: '/contacts',
-  create: '/create',
-  home: '/',
-  results: '/results',
-  survey: '/survey'
+  contacts: `${process.env.REACT_APP_BASE_ROUTE}contacts`,
+  create: `${process.env.REACT_APP_BASE_ROUTE}create`,
+  home: `${process.env.REACT_APP_BASE_ROUTE}`,
+  results: `${process.env.REACT_APP_BASE_ROUTE}results`,
+  survey: `${process.env.REACT_APP_BASE_ROUTE}survey`
 };
 
 export { ROUTES as default };
