40da6828dc6f2a6aca28bdc50c2d6befd1f5e294
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-05-01 11:55:38 +0400

    Fix Create survey width bug for large screen in CreateSurveyBlock.style.js

--- src/Components/CreateSurveyBlock/CreateSurveyBlock/CreateSurveyBlock.style.js
+++ src/Components/CreateSurveyBlock/CreateSurveyBlock/CreateSurveyBlock.style.js
@@ -5,11 +5,7 @@ export const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     boxSizing: 'border-box',
-    paddingTop: `${theme.customSpacing.small}`,
-    width: '100%',
-    '@media only screen and (min-width: 600px)': {
-      padding: `${theme.customSpacing.base}`
-    }
+    paddingTop: `${theme.customSpacing.small}`
   },
   blockTitleWrapper: {
     display: 'flex',
