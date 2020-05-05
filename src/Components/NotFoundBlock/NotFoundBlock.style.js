eb0901098123e2be79d777e9f907c03348b2ad5d
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-05-05 17:04:22 +0400

    Refactor margin top in NotFoundBlock.style.js

--- src/Components/NotFoundBlock/NotFoundBlock.style.js
+++ src/Components/NotFoundBlock/NotFoundBlock.style.js
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
   notFoundBlockContainer: {
-    textAlign: 'center'
+    textAlign: 'center',
+    marginTop: `${theme.customSpacing.xxxLarge}`
   },
   card: {
     display: 'flex',
